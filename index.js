517f621cc13a2fe44d5e023db1ec680c247ab947
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-04-05 15:09:01 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  return word.toLowerCase() === reversedWord.toLowerCase();
 }
 
 /* 
   Add your pseudocode here
+  Initialize empty array called 'reversedArray'
+  Iterate over each character in the input word
+  After iteration 'reversedArray' is converted back to a string
+  if(reversedWord === word);
+  return true else false
 */
 
 /*
   Add written explanation of your solution here
+  The function converts the string to lowercase
+  It then turns the string into an array of characters
+  Reverses the array with built-in method reverse()
+  Then turns array of characters back to a string using join ()
+  It then compares the recent string with the original string using '==='
+  Returns true if string is palindrome else false 
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +29,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
